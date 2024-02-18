1a6d7176987f1720cf9dc2a74061e1378da7ea95
Matías Di Bernardo <matias.di.bernardo@hotmail.com>
2024-02-18 17:37:11 -0300

    Fix text id to pass test based on modifications

--- test/test_grabador.py
+++ test/test_grabador.py
@@ -15,17 +15,21 @@ def create_user(client, app):
 
 def test_new_user_correctly_adds_to_db(client, app):
     id_user = create_user(client, app)
-    response = client.post(f"/recording/{id_user}", data=
-        {"file": None,
-         "autor": None})
+    audio_test_path_1 = "website\\static\\audios\\0.wav"
+
+    # Send audio post to the backend
+    with open(audio_test_path_1, 'rb') as audio_file:
+        response = client.post(f"/recording/{id_user}", data=
+            {"file": audio_file,
+            "author": None})
     
-    print(response)
-    
+    created_audio_path = f"uploads\{id_user}\{id_user}_Archivoz_6_0.mp3"
     with app.app_context():
         rec_object = Usuario.query.first().grabaciones 
-        print(rec_object)
-        #assert rec_object[0].text_id[0] == "Archivoz_4_0"
-        assert len(rec_object) == 0
+
+        assert rec_object[0].text_id == "Archivoz_6_0"
+        assert rec_object[0].usuario_id == id_user
+        assert rec_object[0].audio_path == created_audio_path
 
 def test_bad_input_by_the_user(client, app):
     id_user = create_user(client, app)
